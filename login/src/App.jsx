0f0a3b234d31b1c13c1dff37b41baf8b9e4f050a
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-05-07 15:01:26 +0530

    done

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {Login} from "./Components/Login"
+import {SignUp} from "./Components/SignUp"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Login/>
+      <SignUp/>
     </div>
   )
 }

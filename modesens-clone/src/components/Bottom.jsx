8562e2853ff75dc709cca0f126effcded9fa3817
Priyankadubey190 <priyankadubey0811@gmail.com>
2022-05-07 16:01:38 +0530

    second  pd commit

--- modesens-clone/src/components/Bottom.jsx
+++ modesens-clone/src/components/Bottom.jsx
@@ -10,17 +10,19 @@ export const Bottom = ()=>{
                 <p>A seamless experience that takes your style</p>
                 <p> to the next level</p>
                 <button className="btn">DOWNLOAD NOW</button>
-
-                <img src="https://cdn.modesens.com/static/img/20220420AppDownloaden.png" alt="" />
+                  <br />
+                  <br />
+                <img src="https://cdn.modesens.com/static/img/20220420AppDownloaden.png" alt="" height="400px" width="300px" />
                 </div>
                 <div className="right">
                 <h1>Install the ModeSens</h1>
                 <h1>Browser Extension</h1>
                 <p>Get timely, accurate product information</p>
                 <p>exery time you browse</p>
-                <button>INSTALL NOW</button>
-
-                <img src="https://cdn.modesens.com/static/img/20211109downloadright.png" alt="" />
+                <button className="btn">INSTALL NOW</button>
+                <br />
+                <br />
+                <img src="https://cdn.modesens.com/static/img/20211109downloadright.png" alt=""  height="360px" width="300px"  />
                 </div>
             </div>
         </div>
--- modesens-clone/src/components/Trending.jsx
+++ modesens-clone/src/components/Trending.jsx
@@ -3,7 +3,7 @@ import "react-caroussel/dist/index.css";
 import "../style/trending.css";
 import { branditem } from "../data/ima";
 const Card = ({ ele }) => (
-  <div className="card" style={{border: "1px solid red"}}>
+  <div className="card">
     <img src={ele.src} alt="img" height="300px" width="100%" />
     {/* <h1>{ele.heading}</h1>
     <p>
@@ -15,7 +15,7 @@ const Card = ({ ele }) => (
 
 export const Trending = ()=> {
   return (
-    <div className="container" style={{border:"1px solid red"}}>
+    <div className="container">
       <ReactCaroussel slidesToShow={4} slidesToScroll={1} infinite={true}>
         {branditem
           .map((ele, index) => (

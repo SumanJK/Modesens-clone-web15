ce532536c9c4f19d8210de83c955b428749d0dd4
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-05-08 17:29:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
-import { HeadingBeauty } from './components/HeadingBeauty'
-import{Product} from "./components/Products"
-import {PaginationSize} from "./components/PaginationOn"
-import{FilterDiv} from "./components/FilterDiv"
+// import { HeadingBeauty } from './components/HeadingBeauty'
+// import{Product} from "./components/Products"
+// import {PaginationSize} from "./components/PaginationOn"
+// import{FilterDiv} from "./components/FilterDiv"
+import Products from './pages/Products'
 
 
 function App() {
@@ -13,16 +14,7 @@ function App() {
   return (
     <div className="App">
       
-      <div className='container'>
-
-       <HeadingBeauty/>
-       <div className='main'>
-         <div className='fillter'><FilterDiv/></div>
-         <div className='products'><Product/> </div>
-       </div>
-       
-    
-      </div>
+     <Products/>
 
 
 
